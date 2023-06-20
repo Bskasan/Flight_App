2279cb64472ad07c917830528d1c44341b8c05ba
Bskasan <b.kasan@yandex.com>
2023-06-20 19:16:06 +0300

    env if/els - init.py - settings done

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -1,3 +1,12 @@
 # Settings Folder now is module after created init.py.
 from .base import *
 from .development import *
+
+# Accessing ENV from .env document by using python-decouple.
+# It shouldn't be known by user. so that's why we use this parameter in the .evn file.
+ENVIRONMENT = config('ENV')
+
+if ENVIRONMENT=='development':
+    from .development import *
+else:
+    from .production import *
