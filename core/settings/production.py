5a344119bcad41ef5f5d928ac3539ce68847e796
Bskasan <b.kasan@yandex.com>
2023-06-21 17:42:56 +0300

    updated database

--- core/settings/production.py
+++ core/settings/production.py
@@ -16,14 +16,18 @@ MIDDLEWARE += []
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = { 
-    "default": { 
-        "ENGINE": "django.db.backends.postgresql_psycopg2", 
-        "NAME": config("SQL_DATABASE"), 
-        "USER": config("SQL_USER"), 
-        "PASSWORD": config("SQL_PASSWORD"), 
-        "HOST": config("SQL_HOST"), 
-        "PORT": config("SQL_PORT"), 
-        "ATOMIC_REQUESTS": True,
+    # "default": { 
+    #     "ENGINE": "django.db.backends.postgresql_psycopg2", 
+    #     "NAME": config("SQL_DATABASE"), 
+    #     "USER": config("SQL_USER"), 
+    #     "PASSWORD": config("SQL_PASSWORD"), 
+    #     "HOST": config("SQL_HOST"), 
+    #     "PORT": config("SQL_PORT"), 
+    #     "ATOMIC_REQUESTS": True,
+    # }
+      'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 } 
 
